4c67257b98d3d1f4495e6795e111fca1640ffc04
Antariksha Gupta <antariksha02@gmail.com>
2017-02-20 00:38:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 // Change the  main text
 var element = document.getElementById('main-text');
 element.innerHTML = " Changed thru JS !";
+
+//Move the image on click
+var img = document.getElementById('madi');
+
+img.onclick = fumction() {
+    img.style.leftmargin = '100px';
+}
