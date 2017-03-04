f65a6aad1d5754afaf38f14bc542b574bf8c8524
Antariksha Gupta <antariksha02@gmail.com>
2017-03-04 20:34:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/test-db', function(req, res) {
 });
 
 
-app.get('/articles/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
   pool.query('SELECT * FROM articles where title = ' + req.params.articleName, function(err, result){
       if(err) {
           res.status(500).send(err.toString());
